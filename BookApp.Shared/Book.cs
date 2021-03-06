4436da8d9aacf89c3feb639c17323561a2eceee3
BongSeon <hawhawba@gmail.com>
2021-03-07 01:13:31 +0900

    Books 테이블과 일대일로 매핑되는 Book 모델 클래스 생성

---
+++ BookApp.Shared/Book.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookApp.Shared
+{
+    public class BookBase
+    {
+        /// <summary>
+        /// 일련 번호
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "번호")]
+        public int Id { get; set; }
+        /// <summary>
+        /// 책 제목
+        /// </summary>
+        [MaxLength(255)]
+        [Required(ErrorMessage = "책 제목을 입력하세요")]
+        [Display(Name = "책 제목")]
+        [Column(TypeName = "NVarChar(255)")]
+        public string Title { get; set; }
+        /// <summary>
+        /// 책 설명
+        /// </summary>
+        [Display(Name = "책 설명")]
+        public string Description { get; set; }
+    }
+
+    [Table("Books")]
+    public class Book : BookBase
+    {
+        // PM> Install-Package System.ComponentModel.Annotations
+        // PM> Install-Package System.ComponentModel.DataAnnotations
+
+        // Empty
+    }
+}
---
+++ BookApp.SqlServer/Books.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[01_Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Title] NVARCHAR(255) NOT NULL, 
+    [Description] NVARCHAR(MAX) NULL
+)
